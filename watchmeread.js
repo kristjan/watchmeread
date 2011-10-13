415a9a1ba8f0bf62a0cdf8bd584c4e4266b4ff2a
Kristján Pétursson <kristjan@gmail.com>
2011-10-13 09:45:01 -0700

    Basic redirect loop
    This action waits 5 seconds then redirects to the next one in the series. Every
    incoming number is the total time the browser has been waiting for the image to
    load.

---
+++ watchmeread.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express.createServer(express.logger());
+
+var INTERVAL = 5;
+var MS_PER_S = 1000;
+app.get('/watch/:n', function(request, response) {
+  var next = parseInt(request.params.n, 10) + INTERVAL
+  timeout = setTimeout(function() {
+    response.header('Content-Type', 'image/gif');
+    response.redirect('/watch/' + next), 302;
+    clearTimeout(timeout);
+  }, INTERVAL * MS_PER_S);
+});
+
+app.listen(process.env.PORT || 3000)
+
